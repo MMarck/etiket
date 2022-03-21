faccbd6449585dc7009f3b3e84d09a7ba911a617
MR.Marck <maanmuri@espol.edu.ec>
2022-03-21 14:57:29 -0500

    formulario de ingreso medio hecho

--- etiket-app/src/components/HomeBody/HomeBody.js
+++ etiket-app/src/components/HomeBody/HomeBody.js
@@ -1,8 +1,9 @@
 import Button from "../Button"
 import Popup from '../Popup';
-import LoginForm from "../LoginForm";
+import LoginForm from "../LoginForm/LoginForm";
 import { Component} from "react";
 import './HomeBody.css';
+import {Link} from 'react-router-dom';
 
 
 class HomeBody extends Component {
@@ -10,9 +11,16 @@ class HomeBody extends Component {
   render(){
     return (
       <div className='homebody'>
-        <div className="LoginForm">
-          <p>Hola</p>
+        <div className="d-flex flex-column justify-content-center align-items-center flex-grow-1 flex-shrink-0 w-50">
+          
+          <img src="./images/solinalLogo.png" width={'200px'} />
+
+          <span id="avisoDeIngreso"><b>Ingresa a Solinal</b> Etiqueta</span>
+
+          <LoginForm/>
+
         </div>
+
         <div className="info" style={{backgroundImage:'url(/images/fondo-web.png)'}} >
           <p>Hola</p>
         </div>
