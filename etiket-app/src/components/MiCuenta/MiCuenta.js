325bf393fc67949ba4a728a162e54ee3bd689fd6
Marck.MR <maanmuri@espol.edu.ec>
2022-03-21 01:58:03 -0500

    dashboard

---
+++ etiket-app/src/components/MiCuenta/MiCuenta.js
@@ -0,0 +1,12 @@
+
+
+
+function MiCuenta(){
+    return(
+      <div id='PaperWelcome' className='d-flex justify-content-center align-items-center flex-column flex-grow-1'>
+        <h1>Mi cuenta</h1>
+      </div>
+    );
+  }
+
+export default MiCuenta;
---
+++ etiket-app/src/components/MisEtiquetas/MisEtiquetas.js
@@ -0,0 +1,9 @@
+function MisEtiquetas(){
+    return(
+      <div id='PaperWelcome' className='d-flex justify-content-center align-items-center flex-column flex-grow-1'>
+        <h1>Mis Etiquetas</h1>
+      </div>
+    );
+  }
+
+export default MisEtiquetas;
