f52f4edb47f939203318e94dc214e953c2942ca4
frajordo <frajordo@espol.edu.ec>
2022-03-21 16:51:12 -0500

    Parte derecha página principal

--- etiket-app/src/components/HomeBody/HomeBody.js
+++ etiket-app/src/components/HomeBody/HomeBody.js
@@ -14,7 +14,35 @@ class HomeBody extends Component {
           <p>Hola</p>
         </div>
         <div className="info" style={{backgroundImage:'url(/images/fondo-web.png)'}} >
-          <p>Hola</p>
+          <h1 className="headText">El generador de etiquetas para alimentos y bebidas</h1>
+          <div style={{marginTop: "50px"}}>
+            <p className="text">
+              Los fabricantes de alimentos y bebidas necesitan diseñar, imprimir y gestionar muchos tipos de etiquetas.
+              Comprendemos que se puede volver tedioso revisar la normativa de cada producto y crear una etiqueta,
+              calcular los nutrientes y diseñar una etiqueta nutricional acorde al país de comercialización para cumplir las
+              últimas tendencias del mercado y las exigencias legales.
+            </p>
+            <p className="text">
+              Es por tal motivo, que ofrecemos una solución amigable para ayudar a simplificar todo el proceso de etiquetado,
+              empezando con la creación del prototipo de una etiqueta de alimentos que cumpla la normativa local o del paísal que desea exportar.
+            </p>
+          </div>
+          <div className="infoSquare">
+            <img src="/images/icons/label.png" className="imgInfoIcon"></img>
+            <p className="squareText">Cree e imprima fácilmente etiquetas para alimentos, suplementos, bebidas alchólicas y no alcohólicas.</p>
+          </div>
+          <div className="infoSquare">
+            <img src="/images/icons/gavel.png" className="imgInfoIcon"></img>
+            <p className="squareText">Diseñe etiquetas nutricionales espectaculares que cumplan con las normativas de la UE y la FDA.</p>
+          </div>
+          <div className="infoSquare">
+            <img src="/images/icons/wheat.png" className="imgInfoIcon"></img>
+            <p className="squareText">La función gestión de textos etiquetados destaca los alérgenos de manera automática cuando aparecen en las listas de ingredientes</p>
+          </div>
+          <div className="verPlanes">
+            <h1 className="highText" style={{marginTop:"40px"}}>VER PLANES</h1>
+            <img src="/images/icons/flecha-hacia-abajo.png" className="highImgIcon"></img>
+          </div>
         </div>
       </div>
     )
