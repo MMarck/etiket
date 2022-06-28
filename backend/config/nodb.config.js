fc45bd4d7c6ba94268458afd87914759efe6c3f9
MR.Marck <maanmuri@espol.edu.ec>
2022-06-28 11:11:09 -0500

    just front at root

--- backend/config/nodb.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  HOST: "cluster0.ruyrqfr.mongodb.net",
-  USER: "etiket_mongoDB",
-  PASSWORD: "gbxBhIqMc7dE2q0E",
-  noDB: "SolinalEtiquetaDB",
-  dialect: "mongodb"
-};
--- backend/models/refreshTokensModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
-
-var refreshTokensSchema = new Schema({
-	'token' : String,
-	"email" : String
-});
-
-module.exports = mongoose.model('refreshTokens', refreshTokensSchema, "refreshTokens");
