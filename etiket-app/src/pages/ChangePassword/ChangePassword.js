f82f076e65a42bf9182e0cdb6f7b490e808be9e9
wesleybriones <wesgbrio@espol.edu.ec>
2022-07-11 22:05:30 -0500

    config icon eye with useState visibility.

--- etiket-app/src/pages/ChangePassword/ChangePassword.js
+++ etiket-app/src/pages/ChangePassword/ChangePassword.js
@@ -1,11 +1,33 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+
 import './ChangePassword.css';
 
 /*
  *   Vista para hacer el cambio de contraseña
  */
 export default function ChangePassword() {
+  const passValues = {
+    password: "",
+    newPassword: "",
+  };
+
+  
+  const [visibility, setVisibility] = useState(false);
+  const [newVisibility, setNewVisibility] = useState(false);
+
+  
+  const onInputPassword = () => {
+    setVisibility(prevState => !prevState);
+  }
+
+  const onInputNewPassword = () => {
+    setNewVisibility(prevState => !prevState);
+  }
+
+
   return (
     <div className="columnContainer w-50">
       <h4>
@@ -13,27 +35,51 @@ export default function ChangePassword() {
       </h4>
       <br />
 
-      <div className="rounded-square mt-5 w-75" style={{ margin: '0px auto' }}>
-        <form className="w-100">
+      <div className="rounded-square mt-5 w-75" style={{ margin: "0px auto" }}>
+        <form className="w-100" >
           <label htmlFor="password">Nueva contraseña</label>
           <br />
           <input
             className="inputText ligth-input mb-4 w-100"
             id="password"
-            type="password"
+            type={
+              visibility ? "text":"password"
+            }
             name="password"
           />
+          <i className='btn-visibility' onClick={ onInputPassword }> 
+              { visibility? <AiOutlineEyeInvisible />:
+                  <AiOutlineEye/>
+              }
+          </i>
+          
+          <br />
+          <label htmlFor="newPassword">Verifica la nueva contraseña</label>
+          <br />
+          <input
+            className="inputText ligth-input mb-4 w-100"
+            id="newpassword"
+            type={
+              newVisibility ? "text":"password"
+            }
+            name="newpassword"
+          />
+          <i className='btn-visibility' onClick={ onInputNewPassword }> 
+              { newVisibility? <AiOutlineEyeInvisible />:
+                  <AiOutlineEye/>
+              }
+          </i>
         </form>
       </div>
 
-      <Link to="/miCuenta">
+      
         <button
-          type="button"
           className=" btn-secondary darkButton fw-bolder p-2 my-4"
-          style={{ width: 'fit-content' }}>
+          style={{ width: "fit-content" }}
+        >
           ACTUALIZAR CONTRASEÑA
         </button>
-      </Link>
+      
     </div>
   );
 }
